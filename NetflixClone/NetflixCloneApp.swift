b1f2238168cc954dd1729d3622b76f45332e7a1a
Mythrai Boga <mythrai.boga@walmart.com>
2025-05-28 18:10:04 +0530

    Initial Commit

---
+++ NetflixClone/NetflixCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixCloneApp.swift
+//  NetflixClone
+//
+//  Created by Mythrai Boga on 28/05/25.
+//
+
+import SwiftUI
+
+@main
+struct NetflixCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
